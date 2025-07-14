589d036d646e6e381d5caea8b8d27b58e46191d5
unknown <terzifurkan34@gmail.com>
2025-07-14 15:56:49 -0700

    OnionArch

---
+++ ProductApp/src/Core/ProductApp.Application/Mapping/GeneralMapping.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ProductApp.Application.Features.Commands.CreateProduct;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Application.Mapping
+{
+    public class GeneralMapping: Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Domain.Entities.Product, dto.ProductViewDto>()
+                .ReverseMap();
+
+            CreateMap<Domain.Entities.Product, CreateProductCommand>()
+                .ReverseMap();
+        }
+    }
+}
